4ec3018e89b7bc349026db6730a8f987573c6a34
QuACS <github@quacs.org>
2021-03-30 01:39:08 +0000

    Tue Mar 30 01:39:08 UTC 2021

---
+++ fall2021/precache-manifest.8209880032f4eb264753b3876bfa6b90.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d5fce1f0680a508e1e94cd5f659552",
+    "url": "/fall2021/c26de12ca6e4aa7ba504.module.wasm"
+  },
+  {
+    "revision": "254f2b09faf2498b897f436de579e07e",
+    "url": "/fall2021/ce8a9eb726637366fbdc.worker.js"
+  },
+  {
+    "revision": "659b651da2586d0427fcc90c63d3e802",
+    "url": "/fall2021/chunk-cfd8699e.ce8a9eb726637366fbdc.worker.js"
+  },
+  {
+    "revision": "0e9c4f3ad178598dc99c",
+    "url": "/fall2021/css/app.1a2edd10.css"
+  },
+  {
+    "revision": "27e65bb5ca1fdd701454",
+    "url": "/fall2021/css/chunk-45ce7d24.e30b7601.css"
+  },
+  {
+    "revision": "dd85610145a8e3c87b76",
+    "url": "/fall2021/css/chunk-9d6fb3ca.27e1866e.css"
+  },
+  {
+    "revision": "c013aad71d387b17a88c",
+    "url": "/fall2021/css/chunk-vendors.42afba86.css"
+  },
+  {
+    "revision": "767a48f0ff5e53735106b149ce26a801",
+    "url": "/fall2021/docker-image.jpg"
+  },
+  {
+    "revision": "7a627b96b09d93ae7ef10e72f209c27b",
+    "url": "/fall2021/img/academic_discord/desktop.png"
+  },
+  {
+    "revision": "54fd347fb67d94a9899b8571e2237490",
+    "url": "/fall2021/img/academic_discord/mobile.png"
+  },
+  {
+    "revision": "ef8b2918d75dbc6f8614066b84eb00ac",
+    "url": "/fall2021/img/academic_discord/tablet.png"
+  },
+  {
+    "revision": "68df788c10af916289eeffd42ffacb09",
+    "url": "/fall2021/img/cait_for_gm/desktop.png"
+  },
+  {
+    "revision": "90c573bd46194d587f66dc4227093a86",
+    "url": "/fall2021/img/cait_for_gm/mobile.png"
+  },
+  {
+    "revision": "5c17c931cf17dd7989eefdfc27483870",
+    "url": "/fall2021/img/cait_for_gm/tablet.png"
+  },
+  {
+    "revision": "6ef046eb4956708d88ae55b9b4d76f17",
+    "url": "/fall2021/img/college_truckers/desktop.png"
+  },
+  {
+    "revision": "06bdbf53e9f87d7d80716fbb39db23d5",
+    "url": "/fall2021/img/college_truckers/mobile.png"
+  },
+  {
+    "revision": "1d6bf976e6f86f66ec71383e28b5923a",
+    "url": "/fall2021/img/college_truckers/tablet.png"
+  },
+  {
+    "revision": "0544db9197f51696206661b4dbc39cc5",
+    "url": "/fall2021/img/quacs_logo_white_duck.0544db91.svg"
+  },
+  {
+    "revision": "751542e9ccd54f292cc0c2d753774b90",
+    "url": "/fall2021/index.html"
+  },
+  {
+    "revision": "0e9c4f3ad178598dc99c",
+    "url": "/fall2021/js/app.9f59b293.js"
+  },
+  {
+    "revision": "82be664b72fa1830d490",
+    "url": "/fall2021/js/chunk-2d0b5f3a.cd88b8a1.js"
+  },
+  {
+    "revision": "ac3aa03a2fbe58baddad",
+    "url": "/fall2021/js/chunk-2d0c15d6.7f06cf09.js"
+  },
+  {
+    "revision": "1c60f1e42a33d6bee5f0",
+    "url": "/fall2021/js/chunk-2d0d6d26.01191229.js"
+  },
+  {
+    "revision": "3ad40c5de2895ff9e4de",
+    "url": "/fall2021/js/chunk-2d0db20a.693fcab7.js"
+  },
+  {
+    "revision": "27e65bb5ca1fdd701454",
+    "url": "/fall2021/js/chunk-45ce7d24.806946a4.js"
+  },
+  {
+    "revision": "dd85610145a8e3c87b76",
+    "url": "/fall2021/js/chunk-9d6fb3ca.389920b4.js"
+  },
+  {
+    "revision": "c013aad71d387b17a88c",
+    "url": "/fall2021/js/chunk-vendors.f8e9e3a3.js"
+  },
+  {
+    "revision": "93b702478aef9bcd91184e1b555d7666",
+    "url": "/fall2021/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fall2021/robots.txt"
+  },
+  {
+    "revision": "18568d0f73ab796241b55ae64b09faa0",
+    "url": "/fall2021/umami.js"
+  }
+]);
--- fall2021/service-worker.js
+++ fall2021/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fall2021/precache-manifest.8a6457eccb37c4aa1f0f82139caeccb2.js"
+  "/fall2021/precache-manifest.8209880032f4eb264753b3876bfa6b90.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "QuACS"});
