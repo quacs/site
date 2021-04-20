869f74b48843eb0f097e3aa0dcbfae9eac729a6a
QuACS <github@quacs.org>
2021-04-20 03:20:45 +0000

    Tue Apr 20 03:20:45 UTC 2021

---
+++ summer2021/precache-manifest.b66bcf0f4e6b3b9b6163b83985951d73.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51640634499b40417a9a",
+    "url": "/summer2021/aa629cea00937f0731fc.module.wasm"
+  },
+  {
+    "revision": "7273fcd33db1331a23bc",
+    "url": "/summer2021/css/app.da840407.css"
+  },
+  {
+    "revision": "0c81b5320ce53ce3f33b",
+    "url": "/summer2021/css/chunk-0d6cb137.27e1866e.css"
+  },
+  {
+    "revision": "acf9076e5651fb5ac737",
+    "url": "/summer2021/css/chunk-1d4fb699.eeefe27b.css"
+  },
+  {
+    "revision": "33eb2c6934476db3f029",
+    "url": "/summer2021/css/chunk-vendors.42afba86.css"
+  },
+  {
+    "revision": "767a48f0ff5e53735106b149ce26a801",
+    "url": "/summer2021/docker-image.jpg"
+  },
+  {
+    "revision": "7a627b96b09d93ae7ef10e72f209c27b",
+    "url": "/summer2021/img/academic_discord/desktop.png"
+  },
+  {
+    "revision": "54fd347fb67d94a9899b8571e2237490",
+    "url": "/summer2021/img/academic_discord/mobile.png"
+  },
+  {
+    "revision": "ef8b2918d75dbc6f8614066b84eb00ac",
+    "url": "/summer2021/img/academic_discord/tablet.png"
+  },
+  {
+    "revision": "0544db9197f51696206661b4dbc39cc5",
+    "url": "/summer2021/img/quacs_logo_white_duck.0544db91.svg"
+  },
+  {
+    "revision": "9f10d2052846b4232511cb53bf6cddd9",
+    "url": "/summer2021/index.html"
+  },
+  {
+    "revision": "7273fcd33db1331a23bc",
+    "url": "/summer2021/js/app.dba5c164.js"
+  },
+  {
+    "revision": "0c81b5320ce53ce3f33b",
+    "url": "/summer2021/js/chunk-0d6cb137.9de8d6e9.js"
+  },
+  {
+    "revision": "acf9076e5651fb5ac737",
+    "url": "/summer2021/js/chunk-1d4fb699.207669be.js"
+  },
+  {
+    "revision": "df32d7821c95fc98d23d",
+    "url": "/summer2021/js/chunk-2d0d744a.576f643c.js"
+  },
+  {
+    "revision": "628de68f5f9af7ed357a",
+    "url": "/summer2021/js/chunk-2d0db20a.56473f0d.js"
+  },
+  {
+    "revision": "0540c24d966ab1831e94",
+    "url": "/summer2021/js/chunk-2d21aeb9.d309844b.js"
+  },
+  {
+    "revision": "ef3a5e67c6d5057b2a3c",
+    "url": "/summer2021/js/chunk-2d228901.f15d83ea.js"
+  },
+  {
+    "revision": "51640634499b40417a9a",
+    "url": "/summer2021/js/chunk-f9a62636.09b6970d.js"
+  },
+  {
+    "revision": "33eb2c6934476db3f029",
+    "url": "/summer2021/js/chunk-vendors.836f2a03.js"
+  },
+  {
+    "revision": "93b702478aef9bcd91184e1b555d7666",
+    "url": "/summer2021/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/summer2021/robots.txt"
+  },
+  {
+    "revision": "18568d0f73ab796241b55ae64b09faa0",
+    "url": "/summer2021/umami.js"
+  }
+]);
--- summer2021/service-worker.js
+++ summer2021/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/summer2021/precache-manifest.7f66a221d9ff84db52287e6e4a9891b8.js"
+  "/summer2021/precache-manifest.b66bcf0f4e6b3b9b6163b83985951d73.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "QuACS"});
