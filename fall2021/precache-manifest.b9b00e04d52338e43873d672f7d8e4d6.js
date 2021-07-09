ade27a166dc8a5995ccb1f05bd4d55cbe664a251
QuACS <github@quacs.org>
2021-07-09 02:38:27 +0000

    Fri Jul  9 02:38:27 UTC 2021

---
+++ fall2021/precache-manifest.b9b00e04d52338e43873d672f7d8e4d6.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f151d689d77400b04d7d187231a1924",
+    "url": "/fall2021/4902a8bcde8916d653f7.worker.js"
+  },
+  {
+    "revision": "3d57caa137357c647c629e8fdfaa453b",
+    "url": "/fall2021/b7e3e704b53ada9f08d9.module.wasm"
+  },
+  {
+    "revision": "447e74c8e99c4ea5a669a6dbed6154ee",
+    "url": "/fall2021/chunk-cfd8699e.4902a8bcde8916d653f7.worker.js"
+  },
+  {
+    "revision": "0b165e8e183fb7d068e3",
+    "url": "/fall2021/css/app.71278e62.css"
+  },
+  {
+    "revision": "e4ba8434125172e5ffbb",
+    "url": "/fall2021/css/chunk-7c901c36.27cbefc4.css"
+  },
+  {
+    "revision": "81953adf107e660f1cfd",
+    "url": "/fall2021/css/chunk-9d6fb3ca.27e1866e.css"
+  },
+  {
+    "revision": "0dd876c068d71ac9f0c4",
+    "url": "/fall2021/css/chunk-vendors.55204a1e.css"
+  },
+  {
+    "revision": "767a48f0ff5e53735106b149ce26a801",
+    "url": "/fall2021/docker-image.jpg"
+  },
+  {
+    "revision": "7a627b96b09d93ae7ef10e72f209c27b",
+    "url": "/fall2021/img/academic_discord/desktop.png"
+  },
+  {
+    "revision": "54fd347fb67d94a9899b8571e2237490",
+    "url": "/fall2021/img/academic_discord/mobile.png"
+  },
+  {
+    "revision": "ef8b2918d75dbc6f8614066b84eb00ac",
+    "url": "/fall2021/img/academic_discord/tablet.png"
+  },
+  {
+    "revision": "0544db9197f51696206661b4dbc39cc5",
+    "url": "/fall2021/img/quacs_logo_white_duck.0544db91.svg"
+  },
+  {
+    "revision": "309355bb899cf9843048b28f4d681d85",
+    "url": "/fall2021/index.html"
+  },
+  {
+    "revision": "0b165e8e183fb7d068e3",
+    "url": "/fall2021/js/app.62ebf386.js"
+  },
+  {
+    "revision": "ab3edd16eb57851c89c8",
+    "url": "/fall2021/js/chunk-2d0b5f3a.eb8081cf.js"
+  },
+  {
+    "revision": "0b3f761b9db7a6e05e1d",
+    "url": "/fall2021/js/chunk-2d0c15d6.3d7dab3b.js"
+  },
+  {
+    "revision": "066c4c386a964e0b7de4",
+    "url": "/fall2021/js/chunk-2d0d6d26.afbccc15.js"
+  },
+  {
+    "revision": "a689c6cef0ab839a9bc0",
+    "url": "/fall2021/js/chunk-2d0db20a.c38427a9.js"
+  },
+  {
+    "revision": "e4ba8434125172e5ffbb",
+    "url": "/fall2021/js/chunk-7c901c36.7c6031fe.js"
+  },
+  {
+    "revision": "81953adf107e660f1cfd",
+    "url": "/fall2021/js/chunk-9d6fb3ca.43ce8b96.js"
+  },
+  {
+    "revision": "0dd876c068d71ac9f0c4",
+    "url": "/fall2021/js/chunk-vendors.8cf2f2b0.js"
+  },
+  {
+    "revision": "93b702478aef9bcd91184e1b555d7666",
+    "url": "/fall2021/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fall2021/robots.txt"
+  },
+  {
+    "revision": "18568d0f73ab796241b55ae64b09faa0",
+    "url": "/fall2021/umami.js"
+  }
+]);
