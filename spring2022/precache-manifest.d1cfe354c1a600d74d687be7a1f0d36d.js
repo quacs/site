6baf0e9954f15379010707d8bb48c40b1bfaa38c
QuACS <github@quacs.org>
2021-11-02 20:31:21 +0000

    Tue Nov  2 20:31:21 UTC 2021

---
+++ spring2022/precache-manifest.d1cfe354c1a600d74d687be7a1f0d36d.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41739557e19ea8122262305ab78a501",
+    "url": "/spring2022/803f99a24ff6f28a2e6d.module.wasm"
+  },
+  {
+    "revision": "95399e2471690df515e05bb77671bc5a",
+    "url": "/spring2022/890b2b948157012e5297.worker.js"
+  },
+  {
+    "revision": "32ba07a80a984a10abf80a72dbfeccb4",
+    "url": "/spring2022/chunk-cfd8699e.890b2b948157012e5297.worker.js"
+  },
+  {
+    "revision": "03009b0def2d8cac5518",
+    "url": "/spring2022/css/app.add9adf7.css"
+  },
+  {
+    "revision": "a034db00fb2d87e76dc2",
+    "url": "/spring2022/css/chunk-6481de82.f3c7c48d.css"
+  },
+  {
+    "revision": "bd32a0e6b88abb9fc428",
+    "url": "/spring2022/css/chunk-9d6fb3ca.27e1866e.css"
+  },
+  {
+    "revision": "49de930980475812c336",
+    "url": "/spring2022/css/chunk-vendors.55204a1e.css"
+  },
+  {
+    "revision": "767a48f0ff5e53735106b149ce26a801",
+    "url": "/spring2022/docker-image.jpg"
+  },
+  {
+    "revision": "7a627b96b09d93ae7ef10e72f209c27b",
+    "url": "/spring2022/img/academic_discord/desktop.png"
+  },
+  {
+    "revision": "54fd347fb67d94a9899b8571e2237490",
+    "url": "/spring2022/img/academic_discord/mobile.png"
+  },
+  {
+    "revision": "ef8b2918d75dbc6f8614066b84eb00ac",
+    "url": "/spring2022/img/academic_discord/tablet.png"
+  },
+  {
+    "revision": "d661e2de3bafeea2b918ad8141a1eee5",
+    "url": "/spring2022/img/bunkabl/desktop.png"
+  },
+  {
+    "revision": "9f54731a89857f2b54b6426a15b02675",
+    "url": "/spring2022/img/bunkabl/mobile.png"
+  },
+  {
+    "revision": "f1c770c0d7dff331ff8f0a9d589b2296",
+    "url": "/spring2022/img/bunkabl/tablet.png"
+  },
+  {
+    "revision": "ef608aaf90d1d47642d4ea247126dea7",
+    "url": "/spring2022/img/quacs_logo_halloween.ef608aaf.svg"
+  },
+  {
+    "revision": "785544210d57f3c6504d43b8298db5ad",
+    "url": "/spring2022/index.html"
+  },
+  {
+    "revision": "03009b0def2d8cac5518",
+    "url": "/spring2022/js/app.d3342788.js"
+  },
+  {
+    "revision": "a2bd1b166dec0d35eb65",
+    "url": "/spring2022/js/chunk-2d0db20a.8219246c.js"
+  },
+  {
+    "revision": "baa3a65837e2693baee8",
+    "url": "/spring2022/js/chunk-2d0df058.d4417342.js"
+  },
+  {
+    "revision": "8d849f0ed1a7c625d276",
+    "url": "/spring2022/js/chunk-2d0e8c63.ac7431fb.js"
+  },
+  {
+    "revision": "1850c9f25c412069b561",
+    "url": "/spring2022/js/chunk-2d20eb67.b544f0d5.js"
+  },
+  {
+    "revision": "a034db00fb2d87e76dc2",
+    "url": "/spring2022/js/chunk-6481de82.9bdd63da.js"
+  },
+  {
+    "revision": "bd32a0e6b88abb9fc428",
+    "url": "/spring2022/js/chunk-9d6fb3ca.e053a942.js"
+  },
+  {
+    "revision": "49de930980475812c336",
+    "url": "/spring2022/js/chunk-vendors.77238001.js"
+  },
+  {
+    "revision": "93b702478aef9bcd91184e1b555d7666",
+    "url": "/spring2022/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/spring2022/robots.txt"
+  },
+  {
+    "revision": "18568d0f73ab796241b55ae64b09faa0",
+    "url": "/spring2022/umami.js"
+  }
+]);
--- spring2022/service-worker.js
+++ spring2022/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spring2022/precache-manifest.0acd927858c4fb0e1d81aefeb9deb0bc.js"
+  "/spring2022/precache-manifest.d1cfe354c1a600d74d687be7a1f0d36d.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "QuACS"});
