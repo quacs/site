c8607b69ce33a3d8709198ef3777ac6b1fba64fe
QuACS <github@quacs.org>
2023-08-24 01:52:29 +0000

    Thu Aug 24 01:52:29 UTC 2023

---
+++ fall2023/precache-manifest.301e8d25b59754704338d029ea2ad46c.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b5f3aa5b34bf780999e7eac24dd8669",
+    "url": "/fall2023/5467cbd5b59d59dd8148.worker.js"
+  },
+  {
+    "revision": "8b651ed99b1fd153c330cdcc95f8c00b",
+    "url": "/fall2023/9ba17070045f4606cabe.module.wasm"
+  },
+  {
+    "revision": "ad7a4ef01f1174f27d9ba6a0587261bb",
+    "url": "/fall2023/chunk-cfd8699e.5467cbd5b59d59dd8148.worker.js"
+  },
+  {
+    "revision": "e0539ef97f12e931c9eb",
+    "url": "/fall2023/css/app.52da4c2c.css"
+  },
+  {
+    "revision": "5e6a470b34b9fde59e2c",
+    "url": "/fall2023/css/chunk-5e34b6a8.1995ff6a.css"
+  },
+  {
+    "revision": "cd85bb739953987cb3f8",
+    "url": "/fall2023/css/chunk-9d6fb3ca.27e1866e.css"
+  },
+  {
+    "revision": "9db4a46a18c8fa270e39",
+    "url": "/fall2023/css/chunk-vendors.55204a1e.css"
+  },
+  {
+    "revision": "767a48f0ff5e53735106b149ce26a801",
+    "url": "/fall2023/docker-image.jpg"
+  },
+  {
+    "revision": "7a627b96b09d93ae7ef10e72f209c27b",
+    "url": "/fall2023/img/academic_discord/desktop.png"
+  },
+  {
+    "revision": "54fd347fb67d94a9899b8571e2237490",
+    "url": "/fall2023/img/academic_discord/mobile.png"
+  },
+  {
+    "revision": "ef8b2918d75dbc6f8614066b84eb00ac",
+    "url": "/fall2023/img/academic_discord/tablet.png"
+  },
+  {
+    "revision": "d661e2de3bafeea2b918ad8141a1eee5",
+    "url": "/fall2023/img/bunkabl/desktop.png"
+  },
+  {
+    "revision": "9f54731a89857f2b54b6426a15b02675",
+    "url": "/fall2023/img/bunkabl/mobile.png"
+  },
+  {
+    "revision": "f1c770c0d7dff331ff8f0a9d589b2296",
+    "url": "/fall2023/img/bunkabl/tablet.png"
+  },
+  {
+    "revision": "395e1533e3a2a3aa85431fe8b37ebe7a",
+    "url": "/fall2023/img/logos/quacs_logo.svg"
+  },
+  {
+    "revision": "69698ca89ce8d260f4ac0ae41add8260",
+    "url": "/fall2023/img/logos/quacs_logo_christmas.svg"
+  },
+  {
+    "revision": "ef608aaf90d1d47642d4ea247126dea7",
+    "url": "/fall2023/img/logos/quacs_logo_halloween.svg"
+  },
+  {
+    "revision": "c707d6e3c7a7dfee3bee4e467d500c5b",
+    "url": "/fall2023/img/logos/quacs_logo_thanksgiving.svg"
+  },
+  {
+    "revision": "05810198fb33f8ed3ce966ac63fefd9b",
+    "url": "/fall2023/img/logos/quacs_logo_white.svg"
+  },
+  {
+    "revision": "0544db9197f51696206661b4dbc39cc5",
+    "url": "/fall2023/img/logos/quacs_logo_white_duck.svg"
+  },
+  {
+    "revision": "cddcb67fb0900618b801372fd7e23661",
+    "url": "/fall2023/index.html"
+  },
+  {
+    "revision": "e0539ef97f12e931c9eb",
+    "url": "/fall2023/js/app.c84e8b67.js"
+  },
+  {
+    "revision": "54db314943069a3995a6",
+    "url": "/fall2023/js/chunk-2d0c951f.975ea2f4.js"
+  },
+  {
+    "revision": "234b6294f95bbea70713",
+    "url": "/fall2023/js/chunk-2d0c9759.81f8c52f.js"
+  },
+  {
+    "revision": "cdabdc4e8146b5876d88",
+    "url": "/fall2023/js/chunk-2d0db20a.6d84fa00.js"
+  },
+  {
+    "revision": "74d2a69ecfa4efad529d",
+    "url": "/fall2023/js/chunk-2d20ec5d.285f0a77.js"
+  },
+  {
+    "revision": "5e6a470b34b9fde59e2c",
+    "url": "/fall2023/js/chunk-5e34b6a8.8a871c78.js"
+  },
+  {
+    "revision": "cd85bb739953987cb3f8",
+    "url": "/fall2023/js/chunk-9d6fb3ca.9bda0f01.js"
+  },
+  {
+    "revision": "9db4a46a18c8fa270e39",
+    "url": "/fall2023/js/chunk-vendors.a85625b0.js"
+  },
+  {
+    "revision": "93b702478aef9bcd91184e1b555d7666",
+    "url": "/fall2023/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fall2023/robots.txt"
+  }
+]);
--- fall2023/service-worker.js
+++ fall2023/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fall2023/precache-manifest.64db2523d5398cf5d58c69e09d1c8528.js"
+  "/fall2023/precache-manifest.301e8d25b59754704338d029ea2ad46c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "QuACS"});
