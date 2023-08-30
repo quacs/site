c56ed263a0a519cd6cef73a8c6bb74ec42a4fd0f
QuACS <github@quacs.org>
2023-08-30 22:21:27 +0000

    Wed Aug 30 22:21:26 UTC 2023

--- fall2023/service-worker.js
+++ fall2023/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fall2023/precache-manifest.7731058a369c42e725643d745bbda887.js"
+  "/fall2023/precache-manifest.8acfd93a8fc06878f48325607072e6da.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "QuACS"});
