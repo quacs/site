764c784325a168a7e77c0365c5fb1e8f24037ef4
QuACS <github@quacs.org>
2024-11-08 20:22:06 +0000

    Fri Nov  8 20:22:06 UTC 2024

--- spring2025/service-worker.js
+++ spring2025/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spring2025/precache-manifest.3aff038a30558a63b731cd196d21aae0.js"
+  "/spring2025/precache-manifest.8932b9c43d73930f4b0a1b9bd34e161d.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "QuACS"});
