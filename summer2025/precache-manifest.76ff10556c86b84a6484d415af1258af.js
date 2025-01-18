ab6f3f8ea0c97d7bafa1b4fc15d5c8af1ab582e0
QuACS <github@quacs.org>
2025-01-18 07:25:24 +0000

    Sat Jan 18 07:25:24 UTC 2025

---
+++ summer2025/precache-manifest.76ff10556c86b84a6484d415af1258af.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f26e85ad877875322b8e6cf876327b",
+    "url": "/summer2025/4d7c33bb7c256f274395.worker.js"
+  },
+  {
+    "revision": "05046bb49989877f9a57b2c3c9d2bfa1",
+    "url": "/summer2025/chunk-cfd8699e.4d7c33bb7c256f274395.worker.js"
+  },
+  {
+    "revision": "3b0355760b2362ffbabd",
+    "url": "/summer2025/css/app.424e2afe.css"
+  },
+  {
+    "revision": "398922971b84834d1c26",
+    "url": "/summer2025/css/chunk-5a5b9097.27e1866e.css"
+  },
+  {
+    "revision": "f3afc084b5036fda3bc6",
+    "url": "/summer2025/css/chunk-780261d8.1995ff6a.css"
+  },
+  {
+    "revision": "eeb29d0afbb41c9e3c84",
+    "url": "/summer2025/css/chunk-vendors.55204a1e.css"
+  },
+  {
+    "revision": "cba14e23dc841a1b1b31d3ecbb07cac3",
+    "url": "/summer2025/d9ed6d7e35714aff4c30.module.wasm"
+  },
+  {
+    "revision": "767a48f0ff5e53735106b149ce26a801",
+    "url": "/summer2025/docker-image.jpg"
+  },
+  {
+    "revision": "395e1533e3a2a3aa85431fe8b37ebe7a",
+    "url": "/summer2025/img/logos/quacs_logo.svg"
+  },
+  {
+    "revision": "69698ca89ce8d260f4ac0ae41add8260",
+    "url": "/summer2025/img/logos/quacs_logo_christmas.svg"
+  },
+  {
+    "revision": "ef608aaf90d1d47642d4ea247126dea7",
+    "url": "/summer2025/img/logos/quacs_logo_halloween.svg"
+  },
+  {
+    "revision": "c707d6e3c7a7dfee3bee4e467d500c5b",
+    "url": "/summer2025/img/logos/quacs_logo_thanksgiving.svg"
+  },
+  {
+    "revision": "05810198fb33f8ed3ce966ac63fefd9b",
+    "url": "/summer2025/img/logos/quacs_logo_white.svg"
+  },
+  {
+    "revision": "0544db9197f51696206661b4dbc39cc5",
+    "url": "/summer2025/img/logos/quacs_logo_white_duck.svg"
+  },
+  {
+    "revision": "de041e3d69dda9b343f47155fde23c52",
+    "url": "/summer2025/index.html"
+  },
+  {
+    "revision": "3b0355760b2362ffbabd",
+    "url": "/summer2025/js/app.f8528c12.js"
+  },
+  {
+    "revision": "5d63d80e77a2284642f0",
+    "url": "/summer2025/js/chunk-2d0a4b8e.24e1475a.js"
+  },
+  {
+    "revision": "cdabdc4e8146b5876d88",
+    "url": "/summer2025/js/chunk-2d0db20a.6d84fa00.js"
+  },
+  {
+    "revision": "ae74b253ebd0352365dc",
+    "url": "/summer2025/js/chunk-2d0f11b6.56c0b620.js"
+  },
+  {
+    "revision": "81ac9619a2f03f78db54",
+    "url": "/summer2025/js/chunk-2d22d606.9337ed13.js"
+  },
+  {
+    "revision": "398922971b84834d1c26",
+    "url": "/summer2025/js/chunk-5a5b9097.e78ef84d.js"
+  },
+  {
+    "revision": "f3afc084b5036fda3bc6",
+    "url": "/summer2025/js/chunk-780261d8.911179bf.js"
+  },
+  {
+    "revision": "eeb29d0afbb41c9e3c84",
+    "url": "/summer2025/js/chunk-vendors.6a7d5674.js"
+  },
+  {
+    "revision": "93b702478aef9bcd91184e1b555d7666",
+    "url": "/summer2025/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/summer2025/robots.txt"
+  }
+]);
--- summer2025/service-worker.js
+++ summer2025/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/summer2025/precache-manifest.6fbb38edaf0519fff7bec07daed4a0f0.js"
+  "/summer2025/precache-manifest.76ff10556c86b84a6484d415af1258af.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "QuACS"});
