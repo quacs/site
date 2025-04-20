ebbd7d61bd09de942dc76f8001cfbf929c9d4a4c
QuACS <github@quacs.org>
2025-04-20 03:16:28 +0000

    Sun Apr 20 03:16:28 UTC 2025

--- summer2025/service-worker.js
+++ summer2025/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/summer2025/precache-manifest.f9f2c5911c4d37d976579609b97ea5de.js"
+  "/summer2025/precache-manifest.0c2bac171824d0915a33ff3025e94ec1.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "QuACS"});
