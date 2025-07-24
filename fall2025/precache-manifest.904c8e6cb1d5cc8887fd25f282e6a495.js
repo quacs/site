08ee4284a7ade8d10c834d911db8a338c3623689
QuACS <github@quacs.org>
2025-07-24 13:47:36 +0000

    Thu Jul 24 13:47:36 UTC 2025

---
+++ fall2025/precache-manifest.904c8e6cb1d5cc8887fd25f282e6a495.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3a644e485646b44daae15f7fc91296",
+    "url": "/fall2025/1273816df0e47bf732e5.worker.js"
+  },
+  {
+    "revision": "c561f8471765509bbebe1bcb1018d6a0",
+    "url": "/fall2025/7109c8e74a7706af2421.module.wasm"
+  },
+  {
+    "revision": "6a6514b98cc57402b65df78d38d0e2be",
+    "url": "/fall2025/chunk-cfd8699e.1273816df0e47bf732e5.worker.js"
+  },
+  {
+    "revision": "fac6e28ae3025a3a0f8a",
+    "url": "/fall2025/css/app.424e2afe.css"
+  },
+  {
+    "revision": "398922971b84834d1c26",
+    "url": "/fall2025/css/chunk-5a5b9097.27e1866e.css"
+  },
+  {
+    "revision": "c92f9971a287ccc2253e",
+    "url": "/fall2025/css/chunk-780261d8.1995ff6a.css"
+  },
+  {
+    "revision": "cdbb3b9ad9e71647db05",
+    "url": "/fall2025/css/chunk-vendors.55204a1e.css"
+  },
+  {
+    "revision": "767a48f0ff5e53735106b149ce26a801",
+    "url": "/fall2025/docker-image.jpg"
+  },
+  {
+    "revision": "395e1533e3a2a3aa85431fe8b37ebe7a",
+    "url": "/fall2025/img/logos/quacs_logo.svg"
+  },
+  {
+    "revision": "69698ca89ce8d260f4ac0ae41add8260",
+    "url": "/fall2025/img/logos/quacs_logo_christmas.svg"
+  },
+  {
+    "revision": "ef608aaf90d1d47642d4ea247126dea7",
+    "url": "/fall2025/img/logos/quacs_logo_halloween.svg"
+  },
+  {
+    "revision": "c707d6e3c7a7dfee3bee4e467d500c5b",
+    "url": "/fall2025/img/logos/quacs_logo_thanksgiving.svg"
+  },
+  {
+    "revision": "05810198fb33f8ed3ce966ac63fefd9b",
+    "url": "/fall2025/img/logos/quacs_logo_white.svg"
+  },
+  {
+    "revision": "0544db9197f51696206661b4dbc39cc5",
+    "url": "/fall2025/img/logos/quacs_logo_white_duck.svg"
+  },
+  {
+    "revision": "459f152b54e77000289e71221c43cb04",
+    "url": "/fall2025/index.html"
+  },
+  {
+    "revision": "fac6e28ae3025a3a0f8a",
+    "url": "/fall2025/js/app.c51f2208.js"
+  },
+  {
+    "revision": "f495936831fe4a5d9863",
+    "url": "/fall2025/js/chunk-2d0d3838.9589d691.js"
+  },
+  {
+    "revision": "cdabdc4e8146b5876d88",
+    "url": "/fall2025/js/chunk-2d0db20a.6d84fa00.js"
+  },
+  {
+    "revision": "f916a9120365e92a39f1",
+    "url": "/fall2025/js/chunk-2d2082c5.a84fca12.js"
+  },
+  {
+    "revision": "6fed0b8dbec6d4abad0d",
+    "url": "/fall2025/js/chunk-2d225fc6.7c9602b4.js"
+  },
+  {
+    "revision": "398922971b84834d1c26",
+    "url": "/fall2025/js/chunk-5a5b9097.e78ef84d.js"
+  },
+  {
+    "revision": "c92f9971a287ccc2253e",
+    "url": "/fall2025/js/chunk-780261d8.acacbcb6.js"
+  },
+  {
+    "revision": "cdbb3b9ad9e71647db05",
+    "url": "/fall2025/js/chunk-vendors.8f722fcf.js"
+  },
+  {
+    "revision": "93b702478aef9bcd91184e1b555d7666",
+    "url": "/fall2025/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fall2025/robots.txt"
+  }
+]);
--- fall2025/service-worker.js
+++ fall2025/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fall2025/precache-manifest.74f1182ab037a6cc4c23dc61564bc69a.js"
+  "/fall2025/precache-manifest.904c8e6cb1d5cc8887fd25f282e6a495.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "QuACS"});
