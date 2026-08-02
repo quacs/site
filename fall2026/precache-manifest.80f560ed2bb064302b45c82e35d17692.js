87b34268d5130fee876e54540a16847a4a912a7c
QuACS <github@quacs.org>
2026-08-02 18:12:18 +0000

    Sun Aug  2 18:12:18 UTC 2026

--- fall2026/precache-manifest.80f560ed2bb064302b45c82e35d17692.js
+++ fall2026/precache-manifest.80f560ed2bb064302b45c82e35d17692.js
@@ -12,7 +12,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fall2026/chunk-cfd8699e.bc7a4a9459348fea9397.worker.js"
   },
   {
-    "revision": "267958a77548837f08a0",
+    "revision": "2e0eadb9d975c7518dd0",
     "url": "/fall2026/css/app.424e2afe.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fall2026/css/chunk-5a5b9097.27e1866e.css"
   },
   {
-    "revision": "0d558f5769da7d28d5f4",
+    "revision": "7fa9eee13835c9f3413d",
     "url": "/fall2026/css/chunk-vendors.55204a1e.css"
   },
   {
@@ -56,20 +56,20 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fall2026/img/logos/quacs_logo_white_duck.svg"
   },
   {
-    "revision": "e1a8f1a48f792c31fd9566ba531fc29e",
+    "revision": "59368d81bc90ac70bd8164df5c7deb7a",
     "url": "/fall2026/index.html"
   },
   {
-    "revision": "267958a77548837f08a0",
-    "url": "/fall2026/js/app.bd5b0f82.js"
+    "revision": "2e0eadb9d975c7518dd0",
+    "url": "/fall2026/js/app.b3712fcb.js"
   },
   {
     "revision": "c092f0538274763bfb57",
     "url": "/fall2026/js/chunk-096b44d2.ad24e5cd.js"
   },
   {
-    "revision": "2bc7cc8e2c1ac28a3ee4",
-    "url": "/fall2026/js/chunk-2d0b2328.cc3a3069.js"
+    "revision": "6ecefa9069d47bc65c49",
+    "url": "/fall2026/js/chunk-2d0b2328.fb0cc7e9.js"
   },
   {
     "revision": "cdabdc4e8146b5876d88",
@@ -88,8 +88,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fall2026/js/chunk-5a5b9097.e78ef84d.js"
   },
   {
-    "revision": "0d558f5769da7d28d5f4",
-    "url": "/fall2026/js/chunk-vendors.82b72e99.js"
+    "revision": "7fa9eee13835c9f3413d",
+    "url": "/fall2026/js/chunk-vendors.c535d867.js"
   },
   {
     "revision": "93b702478aef9bcd91184e1b555d7666",
