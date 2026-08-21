d850bf1835d3552bf0a59ebeb069e06ce757d5c5
QuACS <github@quacs.org>
2026-08-21 19:35:46 +0000

    Fri Aug 21 19:35:46 UTC 2026

--- fall2026/precache-manifest.c5ef7c4b52e794e9f0ed8ea2c3af6489.js
+++ fall2026/precache-manifest.c5ef7c4b52e794e9f0ed8ea2c3af6489.js
@@ -12,7 +12,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fall2026/chunk-cfd8699e.c3bc9fa7bed74bcaaed6.worker.js"
   },
   {
-    "revision": "0107747ec3bdf29d4490",
+    "revision": "8408ba20cd8db89bbf58",
     "url": "/fall2026/css/app.424e2afe.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fall2026/css/chunk-5a5b9097.27e1866e.css"
   },
   {
-    "revision": "dbc4623027b4bf0186b5",
+    "revision": "aba8238cdb61ccbedac8",
     "url": "/fall2026/css/chunk-vendors.55204a1e.css"
   },
   {
@@ -56,20 +56,20 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fall2026/img/logos/quacs_logo_white_duck.svg"
   },
   {
-    "revision": "f680d156d3404433085f553f5676d1e9",
+    "revision": "f96f6da770de5cc61e012f0b283e7791",
     "url": "/fall2026/index.html"
   },
   {
-    "revision": "0107747ec3bdf29d4490",
-    "url": "/fall2026/js/app.de303188.js"
+    "revision": "8408ba20cd8db89bbf58",
+    "url": "/fall2026/js/app.745dbb5d.js"
   },
   {
     "revision": "c092f0538274763bfb57",
     "url": "/fall2026/js/chunk-096b44d2.ad24e5cd.js"
   },
   {
-    "revision": "b496b46ff187a4e3caa8",
-    "url": "/fall2026/js/chunk-2d0b2328.56d58b5a.js"
+    "revision": "7b5b019706a0b3693268",
+    "url": "/fall2026/js/chunk-2d0b2328.e64894cf.js"
   },
   {
     "revision": "cdabdc4e8146b5876d88",
@@ -88,8 +88,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fall2026/js/chunk-5a5b9097.e78ef84d.js"
   },
   {
-    "revision": "dbc4623027b4bf0186b5",
-    "url": "/fall2026/js/chunk-vendors.e83dda96.js"
+    "revision": "aba8238cdb61ccbedac8",
+    "url": "/fall2026/js/chunk-vendors.eb9691b5.js"
   },
   {
     "revision": "93b702478aef9bcd91184e1b555d7666",
--- fall2026/service-worker.js
+++ fall2026/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fall2026/precache-manifest.a49479fc7d80abb294ba678876be87d4.js"
+  "/fall2026/precache-manifest.c5ef7c4b52e794e9f0ed8ea2c3af6489.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "QuACS"});
